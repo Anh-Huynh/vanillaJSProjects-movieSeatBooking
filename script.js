e16c43db4c189038a398a640395407e3b2551e17
Anh-Huynh <anh.hxx@gmail.com>
2022-02-03 21:46:42 -0800

    (add) display seat number

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const seatCountText = document.getElementById('count');
 const totalPriceText = document.getElementById('total');
 const selectedMovie = document.getElementById('movie');
 const movieTitle = document.getElementById('movie-title');
+const movieSeat = document.getElementById('movie-seat');
 
 populateUI();
 
@@ -11,6 +12,8 @@ populateUI();
 function updateSelectedCount() {
   const selectedMoviePrice = +localStorage.getItem('moviePrice');
   const selectedMovideTitle = localStorage.getItem('movieTitle');
+  const seatIds = JSON.parse(localStorage.getItem('seatIds'));
+  console.log(seatIds.toString());
 
   const moviePrice = selectedMoviePrice
     ? selectedMoviePrice
@@ -24,15 +27,20 @@ function updateSelectedCount() {
   seatCountText.innerText = totalSelectedSeats;
   totalPriceText.innerText = `$${moviePrice * totalSelectedSeats}`;
   movieTitle.innerText = selectedMovideTitle;
+  movieSeat.innerText = seatIds.join(' ');
 }
 
-function saveSeatSelection() {
+function saveSeatSelection(e) {
   const selectedSeats = document.querySelectorAll('.row .selected');
 
   const selectedSeatIndex = [...selectedSeats].map((seat) =>
     [...seats].indexOf(seat)
   );
+  const selectedSeatId = [...selectedSeats].map((seat) => seat.dataset.seatid);
+  console.log(selectedSeatId);
+
   localStorage.setItem('seatSelection', JSON.stringify(selectedSeatIndex));
+  localStorage.setItem('seatIds', JSON.stringify(selectedSeatId));
 }
 
 function saveMovieSelection(moviePrice, movieIndex, movieTitle) {
@@ -65,8 +73,8 @@ container.addEventListener('click', (e) => {
     !e.target.classList.contains('occupied')
   ) {
     e.target.classList.toggle('selected');
+    saveSeatSelection(e);
     updateSelectedCount();
-    saveSeatSelection();
   }
 });
 
